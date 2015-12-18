168922b841e9fb5cfd3f5b52ccf16714606c86f5
Jong Eun Lee <yomybaby@yomybaby.com>
2015-12-19 08:01:05 +0900

    1st commit

---
+++ src/scripts/main.js
@@ -0,0 +1,33 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  // cube = require('bespoke-theme-cube'),
+  // voltaire = require('bespoke-theme-voltaire'),
+  nebula = require('bespoke-theme-nebula'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  pdf = require('bespoke-pdf'),
+  bullets = require('bespoke-bullets'),
+  backdrop = require('bespoke-backdrop'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  progress = require('bespoke-progress'),
+  forms = require('bespoke-forms');
+
+// Bespoke.js
+bespoke.from('article', [
+  nebula(),
+  keys(),
+  touch(),
+  pdf(),
+  bullets('.bullet'),
+  backdrop(),
+  scale(),
+  hash(),
+  progress(),
+  forms()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
